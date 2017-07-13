30c73452565631ecbf6e1673dd04ee4bb110a5b9
windyfree16 <lwindyfree@gmail.com>
2017-07-14 00:41:32 +0800

    pull detail
    pull detail

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -8,9 +8,38 @@ Page({
     var postId = options.id
     var detail = postData.postList[postId]
 
+    this.data.currentPostId = postId
+
     this.setData({
       info : detail
     })
-    
+
+    // 取出缓存列表
+    var collection_list = wx.getStorageSync('collection_list');
+    if (collection_list) {
+      // 缓存列表存在，则取出当前文章的收藏状态
+      var collection = collection_list[postId]
+      
+      this.setData({
+        collection  : collection
+      })
+
+    } else {
+      // 缓存列表不存在
+      var collection_list = {}
+      collection_list[postId] = false
+      wx.setStorageSync('collection_list', collection_list)
+    }
+
+  },
+  onCollectionTap: function(event) {
+    var collection_list = wx.getStorageSync('collection_list')
+    var collection = collection_list[this.data.currentPostId]
+    collection = !collection
+    collection_list[this.data.currentPostId] = collection
+    wx.setStorageSync('collection_list', collection_list)
+    this.setData({
+      collection: collection
+    })
   }
 })
